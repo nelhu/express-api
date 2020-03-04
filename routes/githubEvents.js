f2bcb85e6ab37bec87a4bd1036a73f503ceeb4ae
huxuezi <xuezierhu@gmail.com>
2020-03-04 23:44:18 +0800

    fix: update github event

--- routes/githubEvents.js
+++ routes/githubEvents.js
@@ -3,6 +3,7 @@ const { succeed, failed } = require('../utils');
 module.exports = function(app) {
 
   app.post('/github_events', (req, res) => {
+    console.log(res);
     res.json(succeed('ok'));
   });
 
