264a561ca8e417c7563dff1e64064eccddf97711
huxuezi <xuezierhu@gmail.com>
2020-03-06 00:16:56 +0800

    fix: update

---
+++ sh/deploy-aliyun-site-ui.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+project_path="/root/dev/projects/aliyun-site-ui"
+serve_path="/root/site/nginx/static/"
+
+timestamp=$(date "+%Y%m%d%H%M%S")
+
+echo $timestamp
+
+cd $project_path
+
+git pull
+
+yarn install --frozen-lockfile
+
+yarn build
+
+file_name="${timestamp}.release.zip"
+zip -r $file_name ./build/
+
+mv -f $file_name $serve_path
+
+cd $serve_path
+
+unzip $file_name
