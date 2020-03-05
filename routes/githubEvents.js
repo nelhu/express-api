1e842c41fa1e35e8dd8752e6759e8932a27ed0d8
huxuezi <xuezierhu@gmail.com>
2020-03-05 18:52:08 +0800

    fix: add log

--- routes/githubEvents.js
+++ routes/githubEvents.js
@@ -1,9 +1,12 @@
+const chalk = require('chalk');
 const { succeed, failed } = require('../utils');
 
 module.exports = function(app) {
 
   app.post('/github_events', (req, res) => {
     console.log(req.body);
+    console.error(req.body);
+    console.log(chalk.blue(req.body));
     res.json(succeed('ok'));
   });
 
