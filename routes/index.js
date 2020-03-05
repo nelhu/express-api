e52a60c72c5d8fb854d736296886520b3963eb7b
huxuezi <xuezierhu@gmail.com>
2020-03-05 18:43:49 +0800

    fix: add github evnets

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const common = require('./common');
 const upload = require('./upload');
+const githubEvents = require('./githubEvents');
 module.exports = function(app) {
   common(app);
   upload(app);
+  githubEvents(app);
 }
