26acbaf5ee2cdcfdd2170c6a9d33e136e97df2ea
huxuezi <xuezierhu@gmail.com>
2020-03-06 23:06:30 +0800

    fix: update

--- routes/common.js
+++ routes/common.js
@@ -2,7 +2,7 @@ const { succeed, failed } = require('../utils');
 
 module.exports = function(app) {
   app.get('/', (req, res) => {
-    res.json(succeed('welcome to api page, please call me QQ: 547567830'));
+    res.json(succeed('welcome to api page, please call me QQ: 547678930'));
   });
 
   app.get('/time', (req, res) => {
