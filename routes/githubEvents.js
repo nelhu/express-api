2d33ada8f994ebd20972292e77407248d284133e
huxuezi <xuezierhu@gmail.com>
2020-03-06 14:41:44 +0800

    fix: update

--- routes/githubEvents.js
+++ routes/githubEvents.js
@@ -10,29 +10,33 @@ const execFile = util.promisify(process.execFile);
 
 module.exports = function(app) {
 
-  app.post('/github_events', (req, res) => {
-    console.log(chalk.blue(JSON.stringify(req.body)));
-    const {
-      repository,
-      pusher,
-      ref,
-    } = request.body;
-    const isAliyunSiteUI = repository.full_name === 'nelhu/aliyun-site-ui';
-    const [, , branchName] = ref.split('/');
-    const isProduction = branchName === 'master';
-    console.log(isAliyunSiteUI, branchName, isProduction);
+  app.post('/github_events', (req, res, next) => {
+    try {
+      console.log(chalk.blue(JSON.stringify(req.body)));
+      const {
+        repository,
+        pusher,
+        ref,
+      } = req.body;
+      const isAliyunSiteUI = repository.full_name === 'nelhu/aliyun-site-ui';
+      const [, , branchName] = ref.split('/');
+      const isProduction = branchName === 'master';
+      console.log(isAliyunSiteUI, branchName, isProduction);
 
-    if (isAliyunSiteUI && isProduction) {
-      const shellPath = path.resolve(__dirname, '../sh/deploy-aliyun-site-ui.sh');
-      console.log(shellPath);
-      chmod(shellPath, '777')
-        .then(() => {
-          return execFile(shellPath)
-          .then(stdout => console.log(stdout))
-          .catch(error => console.error(error))
-        })
+      if (isAliyunSiteUI && isProduction) {
+        const shellPath = path.resolve(__dirname, '../sh/deploy-aliyun-site-ui.sh');
+        console.log(shellPath);
+        chmod(shellPath, '777')
+          .then(() => {
+            return execFile(shellPath)
+            .then(stdout => console.log(stdout))
+            .catch(error => console.error(error))
+          })
+      }
+      res.json(succeed('ok'));
+    } catch (error) {
+      next(error);
     }
-    res.json(succeed('ok'));
   });
 
 }
